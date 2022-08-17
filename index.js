0a1294b83933b0f19bd92f2830bd91bc525b19db
quitovsky <quitovsky@vk.com>
2022-08-17 13:13:14 +0300

    Optimization, fixes & added /reset

--- index.js
+++ index.js
@@ -13,22 +13,30 @@ function ifTableExists(tableName, callback) {
 }
 
 function insertWord(chat, text) {
-    db.all(`SELECT COUNT(word) FROM "${chat}" WHERE "word"="${text}"`, (err, rows) => {
+    db.run(`INSERT OR IGNORE INTO "${chat}" VALUES ("${text}")`)
+}
+
+function sendRandomMessage(ctx, limit) {
+    db.all(`SELECT word FROM "${ctx.chat.id}" ORDER BY RANDOM() LIMIT ${limit}`, (err, rows) => {
         if(err) return console.log(err);
-        const count = rows[0]["COUNT(word)"]
-        if(count === 0) {
-            db.run(`INSERT INTO "${chat}" VALUES ("${text}", 1)`)
-        } else {
-            db.run(`UPDATE "${chat}" SET "count"=${count+1} WHERE "word"="${text}"`)
-        }
+        let msg = rows.map((item) => item.word).join(" ");
+        if(msg.endsWith(",")) msg = msg.slice(0, -1);
+        msg = msg.slice(0,1).toUpperCase() + msg.slice(1);
+        if(msg.length === 0) return;
+        ctx.reply(msg)
     })
 }
 
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(token);
 
-bot.start(async (ctx) => {
-    await ctx.reply("ку")
+bot.use(async(ctx, next) => {
+    if(ctx.chat.type === "group") {
+        ctx.sendRandom = (limit) => {
+            sendRandomMessage(ctx, limit)
+        }
+    }
+    next();
 })
 
 bot.on('my_chat_member', async (ctx) => {
@@ -42,7 +50,7 @@ bot.on('my_chat_member', async (ctx) => {
 
 bot.command("reg", async(ctx) => {
     if(ctx.chat.type === "group") {
-        db.run(`CREATE TABLE IF NOT EXISTS "${ctx.chat.id}" ("word" TEXT, "count" INTEGER)`, (res, err) => {
+        db.run(`CREATE TABLE IF NOT EXISTS "${ctx.chat.id}" ("word" TEXT, PRIMARY KEY (word))`, (res, err) => {
             if(err) ctx.reply("Что-то пошло не так. (ошибка при запросе к базе данных)");
             else ctx.reply("Чат занесен в базу данных, теперь сообщения будут обрабатываться ботом.")
         });
@@ -55,21 +63,9 @@ bot.command("say", async(ctx) => {
         ifTableExists(chat, () => {
             let arg = ctx.message.text.split(" ")[1]
             if(isNaN(arg)) {
-                db.all(`SELECT word FROM "${chat}" ORDER BY RANDOM() LIMIT 15`, (err, rows) => {
-                    if(err) return console.log(err);
-                    let msg = rows.map((item) => item.word).join(" ");
-                    if(msg.endsWith(",")) msg = msg.slice(0, -1);
-                    msg = msg.slice(0,1).toUpperCase() + msg.slice(1);
-                    ctx.reply(msg)
-                })
+                ctx.sendRandom(15);
             } else {
-                db.all(`SELECT word FROM "${chat}" ORDER BY RANDOM() LIMIT ${arg}`, (err, rows) => {
-                    if(err) return console.log(err);
-                    let msg = rows.map((item) => item.word).join(" ");
-                    if(msg.endsWith(",")) msg = msg.slice(0, -1);
-                    msg = msg.slice(0,1).toUpperCase() + msg.slice(1);
-                    ctx.reply(msg)
-                })
+                ctx.sendRandom(arg);
             }
         })
     }
@@ -86,11 +82,24 @@ bot.command('stat', async(ctx) => {
     }
 })
 
+bot.command('reset', async(ctx) => {
+    const chat = ctx.chat.id;
+    if(ctx.chat.type === "group") {
+        const data = await bot.telegram.getChatAdministrators(ctx.chat.id);
+        if(!data || !data.length) return;
+        if(data.some( item => item.user.id === ctx.from.id )) {
+            db.run(`DELETE FROM "${chat}"`)
+            return await ctx.reply("Записи удалены.")
+        }
+    }
+})
+
 bot.on('message', async(ctx) => {
     const chat = ctx.chat.id;
     if(ctx.message.text && ctx.chat.type === "group" && !ctx.from.is_bot) {
         ifTableExists(chat, () => {
-            const text = ctx.message.text.toLowerCase().replace("\n", " ");
+            let text = ctx.message.text.toLowerCase().replaceAll(/(\n)|(-)|(—)|(,)|(\.)|(\()|(\))/ig, " ");
+            text = text.replaceAll(/\s+/ig, " ");
             const words = text.split(" ");
             if(Math.random() > 0.5) {
                 if(words.length <=5 && words.length > 1) {
@@ -107,13 +116,7 @@ bot.on('message', async(ctx) => {
         })
     }
     if(ctx.message.message_id % 20 === 0) {
-        db.all(`SELECT word FROM "${chat}" ORDER BY RANDOM() LIMIT 5`, (err, rows) => {
-            if(err) return console.log(err);
-            let msg = rows.map((item) => item.word).join(" ");
-            if(msg.endsWith(",")) msg = msg.slice(0, -1);
-            msg = msg.slice(0,1).toUpperCase() + msg.slice(1);
-            ctx.reply(msg)
-        })
+        ctx.sendRandom(5)
     }
 })
 
